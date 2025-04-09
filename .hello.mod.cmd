f090f445734edbf1e800b361c05b90c5601cc808
norma-04 <1845981908@qq.com>
2025-04-10 02:06:45 +0800

    rm some file

--- .hello.mod.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_/home/norma/myproject/01_hello/hello.mod := printf '%s\n'   hello.o | awk '!x[$$0]++ { print("/home/norma/myproject/01_hello/"$$0) }' > /home/norma/myproject/01_hello/hello.mod
